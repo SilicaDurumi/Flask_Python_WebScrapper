41204404a6c8b927f1bbe5133f57ba696035a0fd
Camille Neppy <dorumi714@gmail.com>
2020-11-11 06:02:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  word = word.lower()
+  return render_template("report.html", searchingby = word, text = "papyrus")
+
+app.run(host="0.0.0.0")
